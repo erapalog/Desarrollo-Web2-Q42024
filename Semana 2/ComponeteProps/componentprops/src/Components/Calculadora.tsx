96edb2feeb644f5a50c9bdd15aae60ea59553ef0
sharoncar1121 <109883948+sharoncar1121@users.noreply.github.com>
2024-10-16 22:20:02 -0600

    tarea 1

--- Semana 2/ComponeteProps/componentprops/src/Components/Calculadora.tsx
+++ Semana 2/ComponeteProps/componentprops/src/Components/Calculadora.tsx
@@ -13,9 +13,32 @@ function Suma(a: number,b:number){
     return a+b;
 }
 
+function resta (a: number,b:number){
+  return a-b;
+}
+
+function multiplicacion (a: number,b:number){
+  return a*b;
+}
+
+function factorial(a: number) {
+  let result = 1
+  for (let i = 2; i <= a; i++) {
+    result *= i 
+  }
+  return result
+}
+
+
 function Calculadora(props:Parametros) {
   return (
-    <div>Suma de los valores es {Suma(props.a, props.b)}</div>
+    <>
+      <div>Suma de los valores es {Suma(props.a, props.b)}</div>
+      <div>Resta de los valores es {resta(props.a, props.b)}</div>
+      <div>Multiplicacion de los valores es {multiplicacion(props.a, props.b)}</div>
+      <div>Factorial de {props.a} es {factorial(props.a)}</div>
+      <div>Factorial de {props.b} es {factorial(props.b)}</div>
+    </>
   )
 }
 
